dbe50c9406d9ef1f7c49a2fd473df936803d355c
Nary-Vip <nary2vip@gmai.com>
2023-06-23 14:40:04 +0530

    OpenAI prompt is added

--- querying/bookQuerying.py
+++ querying/bookQuerying.py
@@ -1,9 +1,11 @@
 import os
-from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+import pinecone
+from langchain.llms import OpenAI
 from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
-import pinecone
+from langchain.chains.question_answering import load_qa_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
 
 
 # Setting up the open ai key
@@ -52,4 +54,8 @@ query = "Mention 2 algorithm is used for density estimation" # Hard code for now
 docs = docsearch.similarity_search(query, k=5)
 # Top 5 chunks are selected.
 
-    
+llm = OpenAI(temperature=0.8) # Temperature decides the creativity of the LLM while giving the response
+chain = load_qa_chain(llm, chain_type="stuff")
+
+# The query will be answered by the LLM by sending only those chunks of PDFs that related to the user's query
+chain.run(question=query, input_documents= docs)
